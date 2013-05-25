df74a6fb137c64bd6d6575a2bf7c3714ec53eb52
kris <kris@xoofoo.org>
2013-05-25 20:10:12 +0200

    Small change for custom html block

--- index.php
+++ index.php
@@ -454,16 +454,15 @@ if($config['Embedded Script'] == 'off' || headers_sent() == false) header('conte
 		<p><noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 		<a href="http://disqus.com" class="dsq-brlink">Comments powered by <span class="logo-disqus">Disqus</span></a></p>
 		<?php } ?>
-		
 	</section>
+	<?php echo $config['Custom HTML']; ?>
+	
 <?php if($config['Embedded Script'] == 'off') { ?>
 	<footer>
 		<nav><?php if(!empty($config['Home Page'])) { ?><a rel="tooltip" href="<?php echo $config['Home Page']; ?>" title="Go to the home page">Home</a><?php } ?><?php if(!empty($config['Contact'])) { ?> • <a rel="tooltip" href="<?php echo $config['Contact']; ?>" title="Contact us">Contact</a><?php } ?><?php if(!empty($config['Imprint'])) { ?> • <a rel="tooltip" href="<?php echo $config['Imprint']; ?>" title="Imprint this page">Imprint</a> <?php } ?></nav><?php if(!empty($config['Author'])) { ?><p>Photos by <strong><?php echo $config['Author']; ?></strong> - Copyright © 2013 - All rights reserved.</p><?php } ?>
 		<p>Powered by <a rel="tooltip" href="http://getpagemap.com/pagemap-imagewall/" title="A free web gallery script for portfolio websites"><strong>Pagemap ImageWall</strong></a> adapted by <a rel="tooltip" href="http://www.xoofoo.org/" title="XooFoo Websites"><strong>XooFoo</strong></a></p>
 	</footer>
 <?php } ?>
-
-<?php echo $config['Custom HTML']; ?>
 </div>
 <script>
 	document.write('<script src=js/' +
